e23221f86b319bb5b8cff1619d8bb102aad6b20e
premasekaran <premasekaran08@gmail.com>
2026-07-07 15:49:15 +0530

    Add files via upload

---
+++ python/conn2.py
@@ -0,0 +1,108 @@
+import cv2
+import requests
+from cvzone.HandTrackingModule import HandDetector
+
+CAM_IP = "10.122.163.66"
+
+LCD_IP = "10.122.163.8"
+
+cap = cv2.VideoCapture(
+    f"http://10.122.163.66:81/stream"
+)
+
+print("Opened:", cap.isOpened())
+
+detector = HandDetector(
+    maxHands=1,
+    detectionCon=0.5
+)
+
+last_text = ""
+
+while True:
+
+    success, img = cap.read()
+
+    if not success:
+        continue
+
+    img = cv2.flip(img, 1)
+    img = cv2.resize(img, None, fx=2, fy=2)
+
+    hands, img = detector.findHands(img, draw=True)
+    print(hands)
+
+    text = "No Hand"
+
+    if hands:
+
+        hand = hands[0]
+
+        fingers = detector.fingersUp(hand)
+
+        count = fingers.count(1)
+
+        cv2.putText(
+            img,
+            f"Fingers: {count}",
+            (10,40),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0,255,0),
+            2
+        )
+
+        if count == 0:
+            text = "FIST"
+
+        elif count == 1:
+            text = "ONE"
+
+        elif count == 2:
+            text = "TWO"
+
+        elif count == 3:
+            text = "THREE"
+
+        elif count == 4:
+            text = "FOUR"
+
+        elif count == 5:
+            text = "FIVE"
+
+    cv2.putText(
+        img,
+        text,
+        (10,90),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (255,0,0),
+        2
+    )
+
+    # Send to LCD only if the text changed
+    if text != last_text:
+
+        try:
+            requests.get(
+                f"http://{LCD_IP}/display",
+                params={"text": text},
+                timeout=1
+            )
+
+            print("LCD:", text)
+
+            last_text = text
+
+        except Exception as e:
+            print("ESP32 Error:", e)
+
+    cv2.imshow("ESP32 Gesture Control", img)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
